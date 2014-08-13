7a93266a8cf09fa44756c08b845bfa9485ffe857
Stephen Wong <stephwwong@gmail.com>
2014-08-12 22:06:03 -0400

    First commit based on existing code

---
+++ tweetable.js
@@ -0,0 +1,12 @@
+$(function() {
+  //looks for stuff like <a href="#" class="tweetable">{your tweetable content}</a>
+  $('a.tweetable').each(function(){
+    var content = $(this).html();
+    console.log(content);
+    $(this).attr('href', 'http://twitter.com/home?status=' + content + ' via @wingchi'); //add your twitter handle here
+    //feel free to remove any of the following stuff
+    $(this).attr('target', '_blank'); //open 
+    $(this).attr('style', 'border-bottom: 1px dotted #39cccc;') //a dotted underline for added emphasis
+    $(this).html(content + ' <i class="fa fa-twitter"></i>'); //add a font-awesome twitter icon at the end of your tweetable content
+  });
+});
