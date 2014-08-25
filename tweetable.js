a1ba08656d1d757196bb82c25592325daa19e32b
Stephen Wong <stephwwong@gmail.com>
2014-08-25 17:03:07 -0400

    Added link to tweet.

--- tweetable.js
+++ tweetable.js
@@ -3,8 +3,7 @@ $(function() {
   $('a.tweetable').each(function(){
     var content = $(this).html();
     console.log(content);
-    //TODO - Add a link to the current page to the tweet
-    $(this).attr('href', 'http://twitter.com/home?status=' + content + ' via @wingchi'); //add your twitter handle here
+    $(this).attr('href', 'http://twitter.com/home?status=' + content + ' ' + $(location).attr('href') + ' via @wingchi'); //add your twitter handle here
     //feel free to remove any of the following stuff
     $(this).attr('target', '_blank'); //open 
     $(this).attr('style', 'border-bottom: 1px dotted #39cccc;') //a dotted underline for added emphasis
